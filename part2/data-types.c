97980fff2229f44b7116d04928eb2a4bc34f6441
Julien Delange <julien@gunnm.org>
2020-03-14 20:21:11 -0700

    update data-types

---
+++ part2/data-types.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct person {
+    char* firstname;
+    char* lastname;
+    int   age;
+};
+
+enum class_part {
+    part1 = 1,
+    part2 = 2,
+    part3 = 3
+};
+
+union unsigned_value {
+    unsigned char small;
+    unsigned long big;
+};
+
+int main(int argc, char* argv[]) {
+    struct person teacher;
+    teacher.age = 22;
+
+    printf("the teacher is %d years old\n", teacher.age);
+
+    enum class_part cp;
+    cp = part1;
+
+    union unsigned_value uv;
+    uv.small = 8;
+
+    printf("value = %u\n", uv.small);
+
+    uv.big = 45698987;
+
+    printf("value = %u\n", uv.small);
+    printf("value = %lu\n", uv.big);
+
+    int array[2];
+    array[0] = 2;
+    array[1] = 3;
+
+    int array2[] = { 2 , 3};
+
+    return (EXIT_SUCCESS);
+}
