6770b1b0ddf02d5dace4b7122b514c724a7ded67
Julien Delange <julien@gunnm.org>
2020-03-15 17:28:10 -0700

    add linkedlist example

---
+++ part2/linkedlist.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct node {
+    int value;
+    struct node* next;
+} node_t;
+
+int add_node(node_t* root, node_t toadd) {
+    return 0;
+}
+
+int remove_value(node_t* root, node_t toadd) {
+    return 0;
+}
+
+void print_linked_list(node_t* root) {
+}
+
+int main(int argc, char* argv[]) {
+    exit(EXIT_SUCCESS);
+}
